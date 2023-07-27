03cf20fb8b722c786f7555a6daf53853ed42c4b4
Supun Rathnayake <42246100+supunstr@users.noreply.github.com>
2023-07-28 02:03:58 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,5 +14,11 @@ variable "rules" {
     managed_rule_group_statement_vendor_name = string
     metric_name                              = string
     listasd                                  = optional(list(string))
+    ip_set_reference                         = bool
   }))
+}
+
+variable "rules_contains_ip_set_reference" {
+  type    = bool
+  default = true
 }
